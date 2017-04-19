286f905903ad9544ba04ea5a6f55bc8bea8de5f0
Damien Thenot <spaceship84@gmail.com>
2017-04-19 11:26:57 +0200

    On limite le nombre d'arguments pour le programme aux choses requises, n et k. Plus ajout de quelques affichages de debug.

--- projet_crypto.c
+++ projet_crypto.c
@@ -1,10 +1,28 @@
 #include <gmp.h>
 #include <stdio.h>
+#define ligne printf("\n");
 
 int main(int argc, char* argv[]){
-	printf("Bonjour\n");
-	mpz_t n;
-	mpz_init(n);
-	printf("%d\n", n);
-	mpz_clear(n);
+	
+	/**/ //Affiche les arguments du programme
+	int i = 0;
+	printf("argc : %d", argc); ligne
+	for(i = 0; i<argc; i++){
+		printf("argc[%d] : %s\n", i, argv[i]);
+	}
+	/**/
+	
+	if(argc == 3){
+		mpz_t n;
+		mpz_init(n);
+		printf("%d\n", n);
+		mpz_clear(n);
+		
+		return 0;
+	}
+	else{
+		printf("Vous n'avez pas entrer le bon nombre d'arguments lors du lancement du programme.\n");
+		return 1;
+	}
+	
 }
