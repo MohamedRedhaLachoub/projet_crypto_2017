f5c1eccd12b47cff0b218f51d404cfd94d595430
uvsq21404821 <rom1.robr@gmail.com>
2017-04-21 01:11:45 +0200

    Dams j'ai le plaisir de t'annoncer que le test de fermat est op plusieur test o été réalisé bien sur

--- projet_crypto.c
+++ projet_crypto.c
@@ -1,7 +1,10 @@
 #include <gmp.h>
 #include <stdio.h>
 #include <string.h>
-
+				
+			
+			
+			
 void square_and_multiply(mpz_t res, mpz_t a, mpz_t exp, mpz_t mod){
 	//a^exp mod n
 	mpz_t r; int i;
@@ -9,10 +12,10 @@ void square_and_multiply(mpz_t res, mpz_t a, mpz_t exp, mpz_t mod){
 
 	mpz_init_set(r, a);//On met a dans r
 	binexp = mpz_get_str(NULL, 2, exp); // Transformation de l'exposant au format binaire.
-	printf("binexp : %s\n", binexp);
+	//printf("binexp : %s\n", binexp);
 
 	for(i=1 ; i<strlen(binexp); i++){
-		printf("i=%d et bine=%c",i,binexp[i]);
+		//printf("i=%d et bine=%c/n",i,binexp[i]);// test pour afficher le tableau binaire
 		mpz_mul(r, r, r);
 		mpz_mod(r, r, mod);
 		if(binexp[i] == '1'){
@@ -28,8 +31,36 @@ void square_and_multiply(mpz_t res, mpz_t a, mpz_t exp, mpz_t mod){
 	mpz_t res2;
 	mpz_init(res2);
 	mpz_powm(res2, a, exp, mod);
-	printf("fonction de la gmp : %s\n", mpz_get_str(NULL, 10, res2));
+	//printf("fonction de la gmp : %s\n", mpz_get_str(NULL, 10, res2));
 }
+
+int test_de_fermat(mpz_t n,int k)
+{
+	int i;
+	mpz_t restmp;
+	mpz_init(restmp);
+	mpz_t a;
+	mpz_init(a);
+	mpz_t limite;
+	mpz_init_set(limite, n);
+	mpz_sub_ui(limite,limite,1);
+	gmp_randstate_t state;
+	gmp_randinit_default(state);
+	for(i=1;i<=k;i++)
+	{
+		mpz_urandomm(a,state,limite);//Genere l'entier a aleatoirement entre 0 < a < n-1
+		mpz_add_ui(a,a,1); //Genere l'entier a aleatoirement entre 1 < a < n-1
+		square_and_multiply(restmp,a,limite,n); // Effectue a^n-1 mod n
+			if ( mpz_cmp_ui(restmp,1)!=0) // verifie si le resultat est different de 1
+				{
+					return 0;
+				}
+	}
+	mpz_clear(limite);
+	mpz_clear(restmp);
+	mpz_clear(a);
+	return 1;
+}	
 
 int main(int argc, char* argv[]){
 
@@ -42,28 +73,22 @@ int main(int argc, char* argv[]){
 	*/
 
 	if(argc == 3){
-		mpz_t n, res; int i,k;
+		mpz_t n, res; int k,bool;
 		k = atoi(argv[2]);
-
 
 		//mpz_init(n); //On initialise le n en même temps que lui donner la valeur plus bas.
 		mpz_init(res);
 		mpz_init_set_str(n, argv[1], 10); //Permet ne se pas se limiter par le int maximum pour notre n
-
-
-		printf("%p\n", n);
-		printf("n : %s\n", mpz_get_str(NULL, 10, n)); //Fait passer directement n en string et permet donc de ne pas perdre à l'affichage, ATTENTION le str passer en param est NULL possible fuite de mémoire puisque qu'elle est alloué par GMP
+		gmp_printf("n : %Zd\n",n);
 		printf("k : %d\n", k);
-
 		//mpz_powm_ui(res, n, k, mod); //On n'a pas le droit d'utiliser cette fonction, il faut utiliser square and multiply.
-
-		mpz_t exp, mod;
-		mpz_init_set_ui(exp, k);
-		mpz_init_set_ui(mod, 11);
-
-		square_and_multiply(res, n, exp, mod);
-		printf("result : %s\n", mpz_get_str(NULL, 10, res));
-
+		bool=test_de_fermat(n,k);
+		if(bool)
+		{
+			printf("Test de Fermat = Premier\n");
+		}else{
+			printf("Test de Fermat = Composé\n");
+		}
 		mpz_clear(n);
 		return 0;
 	}
